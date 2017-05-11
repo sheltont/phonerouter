ababaff921391206d346a05d7b8aec33382d8b1d
root <root@i-upo09gbd>
2017-05-11 22:46:46 +0800

    add wav files and fix bugs in routing_server.py

--- routing_server.py
+++ routing_server.py
@@ -23,26 +23,22 @@ logging.getLogger('').addHandler(console)
 
 
 listen_port = 8040
-listen_ip = "127.0.0.1"
-max_sid_len = 'sofia/internal/1007@172.18.100.3:15566'
+listen_ip = '127.0.0.1'
+max_sid_len = 4
 
 logger = logging.getLogger('routing')
-voice_url = '/usr/share/freeswitch/sounds/en/us/callie/voicemail/8000/vm-enter_pass.wav'
+voice_url = '/usr/lib/freeswitch/esl/phonerouter/mj-22.wav'
+#voice_url = '/usr/share/freeswitch/sounds/en/us/callie/voicemail/8000/vm-tutorial_change_pin.wav'
 target_mobile_url = 'sofia/internal/{0}@172.16.29.2:5060'
-
+default_target = 'group/service'
+routing_server = RoutingServer()
 
 class ESLRequestHandler(SocketServer.BaseRequestHandler):
-	def __init__(self, request, client_address, server):
-		self.routing_server = RoutingServer()
-		return
-
-
 	def setup(self):
-		logger.info("%s connected", self.client_address)
+		print self.client_address, ' is connected'
 		fd = self.request.fileno()
 		con = ESL.ESLconnection(fd)
-
-		if con.connected():
+	        if con.connected():
 			info = con.getInfo()
 			uuid = info.getHeader("unique-id")
 			logger.info('the incoming call uuid is: %s', uuid)
@@ -50,36 +46,50 @@ class ESLRequestHandler(SocketServer.BaseRequestHandler):
 			event = con.filter("unique-id", uuid)
 			con.events("plain", "all")
 			con.execute("answer", "", uuid)
-			msg = "%d %d 1 5000 # %s silence_stream://250".format(max_sid_len, max_sid_len, voice_url)
+			msg = "{0} {1} 1 5000 # {2} silence_stream://250".format(max_sid_len, max_sid_len, voice_url)
 			print msg
 			logger.info('play_and_get_digits: %s', msg)
 			con.execute("play_and_get_digits", msg)
 			digits = []
-
+                        see_playback_stop = False
+                        see_server_disconnected = False
 			while con.connected():
 				e = con.recvEvent()
 				if e:
 					name = e.getHeader("event-name")
 					logger.debug("event-name: %s", name)
+                                        print name
 					if name == "DTMF":
 						digit = e.getHeader("dtmf-digit")
 						logger.debug("digit: %s", digit)
 						digits.append(digit)
 						print digit
+                                        elif name == "PLAYBACK_STOP":
+                                                see_playback_stop = True
+                                        elif name == "SERVER_DISCONNECTED":
+                                                see_server_disconnected = True
+                                                break
+                                        elif name == "CHANNEL_EXECUTE_COMPLETE" and see_playback_stop:
+                                                break
 				if len(digits) == max_sid_len:
 					break
 
+                        if see_server_disconnected:
+                                logger.debug("Disconnected")
+                                return
+                        
 			logger.debug('collecting digits: %s', digits)
 			if len(digits) == max_sid_len:
 				sid = ''.join(digits)
 				print sid
-				mobile = self.routing_server.get_mobile(sid)
+				mobile = routing_server.get_mobile(sid)
 				if mobile:
 					target = "{0} XML default".format(mobile)
 					logger.info('transfer the call to %s', target)
 					con.execute("transfer", target, uuid)
-
-			logger.debug("Disconnected!")
+                        else:
+                                print "bridge to the default target ", default_target
+                                con.execute("bridge", default_target, uuid)
 
 
 if __name__ == '__main__':
